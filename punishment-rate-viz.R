36ca733a85924ea7222564a1c6bb0d4f34afb99e
Matt Henry <matthenry@fastmail.fm>
2018-01-28 14:20:25 -0500

    basic dumbbell

--- punishment-rate-viz.R
+++ punishment-rate-viz.R
@@ -1,7 +1,14 @@
 library(dplyr)
+library(ggplot2)
+library(ggalt)
 
 punishment_rate_df <- read.csv("data/punishment_rate.csv", stringsAsFactors = FALSE)
 
-punishment_rate_df <- punishment_rate_df %>%
+punishment_rate_df %>%
   select(-c(Punishment.rank, Imprisonment.rank)) %>%
-  rename(Punishment_Rate = X2013.punishment.rate, Imprisonment_Rate = X2013.imprisonment.rate)
+  rename(Punishment_Rate = X2013.punishment.rate, Imprisonment_Rate = X2013.imprisonment.rate) ->
+  punishment_rate_df
+
+punishment_rate_df %>%
+  ggplot(aes(x = Punishment_Rate, xend = Imprisonment_Rate, y = State)) +
+  geom_dumbbell()
