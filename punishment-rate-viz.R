bb30defd43d60e8c30cc7e752fe0f49061304a7f
Matt Henry <matthenry@fastmail.fm>
2018-01-28 14:10:16 -0500

    use dplyr rename instead of colnames

--- punishment-rate-viz.R
+++ punishment-rate-viz.R
@@ -1,12 +1,7 @@
 library(dplyr)
 
-process_data <- function() {
-  punishment_rate_df <- read.csv("data/punishment_rate.csv", stringsAsFactors = FALSE)
-  
-  punishment_rate_df <- punishment_rate_df %>%
-    select(-c(Punishment.rank, Imprisonment.rank)) 
-  
-  colnames(punishment_rate_df)[c(2,3)] <- c("Punishment_Rate", "Imprisonment_Rate")
-  
-  return(punishment_rate_df)
-}
+punishment_rate_df <- read.csv("data/punishment_rate.csv", stringsAsFactors = FALSE)
+
+punishment_rate_df <- punishment_rate_df %>%
+  select(-c(Punishment.rank, Imprisonment.rank)) %>%
+  rename(Punishment_Rate = X2013.punishment.rate, Imprisonment_Rate = X2013.imprisonment.rate)
