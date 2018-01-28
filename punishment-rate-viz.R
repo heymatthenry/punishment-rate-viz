c2d69dc3254e542f1adcf1ebd0fb6d2675590f38
Matt Henry <matthenry@fastmail.fm>
2018-01-28 17:30:45 -0500

    order data by punishment rate

--- punishment-rate-viz.R
+++ punishment-rate-viz.R
@@ -6,12 +6,16 @@ punishment_rate_df <- read.csv("data/punishment_rate.csv", stringsAsFactors = FA
 
 punishment_rate_df %>%
   select(-c(Punishment.rank, Imprisonment.rank)) %>%
-  rename(Punishment_Rate = X2013.punishment.rate, Imprisonment_Rate = X2013.imprisonment.rate) ->
+  rename(Punishment_Rate = X2013.punishment.rate, 
+         Imprisonment_Rate = X2013.imprisonment.rate) %>%
+  arrange(desc(Punishment_Rate)) -> 
   punishment_rate_df
 
 gg <- ggplot(
             data = punishment_rate_df,
-            aes(x = Punishment_Rate, xend = Imprisonment_Rate, y = State))
+            aes(x = Punishment_Rate, 
+                xend = Imprisonment_Rate, 
+                y = reorder(State, Punishment_Rate)))
 # Colors -----------------#
 # Lighter green : #B8EBD7 #
 # Darker green  : #43BBBF #
